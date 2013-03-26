5cce87cab103b03ef3014f4fc7022e427a59fac1
giusepped <giusdesan@gmail.com>
2013-03-26 21:46:48 +0100

    Create ex5.py

---
+++ ex5.py
@@ -0,0 +1,15 @@
+my_name = "Giuseppe"
+my_age = 26 # not a lie
+my_height = 185 #cm
+my_weight = 97 #kg
+my_eyes = "blue"
+my_hair = "blondish"
+
+print "Let's actually talk about %s." % my_name
+print "He's %d centimeters tall." % my_height
+print "He's %d kilograms heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+
+print "If I add %d, %d, and %d I get %d." % (
+  my_age, my_height, my_weight, my_age + my_height + my_weight)

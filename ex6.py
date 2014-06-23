045c6351fd9b8b389302bc9751874fca3d0847e5
giusepped <giusdesan@gmail.com>
2014-06-23 10:23:03 +0100

    Update ex6.py

--- ex6.py
+++ ex6.py
@@ -6,3 +6,15 @@ y = "Those who know %s and those who %s." % (binary, do_not)
 print x
 print y
 
+print "I said: %r" % 10
+print "I also said: '%s'." % y
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! %r"
+
+print joke_evaluation % hilarious
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+print w + e

9ca915a94fffe63daadc45914ff625c96b4de805
giusepped <giusdesan@gmail.com>
2014-06-25 09:23:15 +0100

    Update ex18.py

--- ex18.py
+++ ex18.py
@@ -1,10 +1,19 @@
 def print_two(*args):
-  arg1, arg2 = args
-  print "arg1: %r, arg2: %r" % (arg1, arg2)
+   arg1, arg2 = args
+   print "arg1: %r, arg2: %r" % (arg1, arg2)
   
 def print_two_again(arg1, arg2):
-  print "arg1: %r, arg2: %r" % (arg1, arg2)
+   print "arg1: %r, arg2: %r" % (arg1, arg2)
   
 def print_one(arg1):
-  print "arg1: %r" % arg1
+   print "arg1: %r" % arg1
+  
+def print_none():
+   print "I got nothin'."
+  
+
+print_two("Giuseppe", "De Santis")
+print_two_again("Giuseppe", "De Santis")
+print_one("One!")
+print_none()
   
